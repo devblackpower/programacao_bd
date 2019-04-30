03d7ed0f1d7f35aacb4af6dcfccd0af590045fe6
Dilenon Delfino <stefan.dilenon@gmail.com>
2019-04-30 19:47:40 -0300

    Create aula10_b.sql

---
+++ aula10_b.sql
@@ -0,0 +1,25 @@
+create or replace
+function func_divisao(pdivisor in number, pdividendo in number)
+return number
+is
+  vresultado number(2);
+  vparametros varchar2(4000);
+  vnumero number(6);
+  vdescrerro varchar2(4000);
+begin
+  vresultado := pdivisor / pdividendo;
+  return vresultado;
+exception
+  when others then
+    rollback;
+    vparametros := 'pdivisor = ' || pdivisor || ','
+                || 'pdividendo = ' || pdividendo;
+    vnumero := sqlcode;
+    vdescrevendo := sqlerrm;
+    insert into log_programacaoexecucao (id_log, nome_do_codigo, parametros, numero_erro,
+      descricao_erro, numero_linha_erro, usuario, datahora, status, descricao_solucao) values
+      (seq_log_progexec.nextval,'func_divisao', vparametros, vnumero, vdescrerro,
+      dbms_utility.format_error_backtrace(), user, sysdate, 'e', null);
+    commit;
+    return sqlcode;
+end;
