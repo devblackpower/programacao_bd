482b8d2cb2ca44dafd37206d995c45e9a5e3b8c1
Dilenon Delfino <stefan.dilenon@gmail.com>
2019-05-07 21:51:10 -0300

    Create aula11_a.sql
    trigger com delete, insert e delete

---
+++ aula11_a.sql
@@ -0,0 +1,9 @@
+create or replace trigger trg_pers_upsal
+before update
+of salario
+on personagem
+for each row
+begin
+    insert into log_personagem( id_log, id, salario_velho, salario_novo, usuario, datahora)
+        values(seq_log_progexec.nextval, :old.id, :old.salario, :new.salario, user, sysdate);
+end;
