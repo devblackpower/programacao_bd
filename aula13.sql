3972c2ac09921897a2fc82e3497c207a2ffb17cd
Dilenon Delfino <stefan.dilenon@gmail.com>
2019-05-28 20:07:59 -0300

    Update aula13.sql

--- aula13.sql
+++ aula13.sql
@@ -1,10 +1,10 @@
-create or replace func_testejob(paumento in number)
+create or replace function func_testejob(paumento in number)
 return number
 is
 
 begin
     update personagem set salario = salario + paumento
-    where i9d = 1;
+    where id = 1;
     commit;
     return 0;
 end;
